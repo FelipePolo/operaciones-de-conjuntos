ad1f8133770058d99055a1557214b2c3b29fe585
Anamariabd <anabuenahoradiaz@gmail.com>
2020-09-28 15:31:37 -0500

    Cambiaré de rama

--- js/ana.js
+++ js/ana.js
@@ -23,31 +23,18 @@ function difference(cadena1, cadena2){
       return !(Lista2.includes(i));
    });
 
+
    return vector;
 }
 
-function ToList(cadena){
-
-   i=0;
-   let list = [];
-   simbolo = "";
-
-   while(i<cadena.length){
-      if(cadena.charAt(i-1)=='{' || cadena.charAt(i-1)==','){
-         j = i;
-         simbolo = "";
-         while(cadena.charAt(j)!=',' && cadena.charAt(j)!='}' && cadena.charAt(j)!='\0'){
-            simbolo = simbolo+cadena.charAt(j);        
-           j++;
-         }
-
-         list.push(simbolo);
-      }
-      i++;
-   }
-
-   return list;
-
+function ToList(cad1, cad2){
+//$("#boton").on("click",function(e){
+   /*los elementos se definen como el valor del imput, luego se ignoran los simbolos "{" y "}" con substring 
+   desde 1 hasta la longitud de la cadena 1 y por ultimo se convierte en un vector dividiendo por la ",".*/
+   var elemento1 = cad1.substring(1,cad1.length-1).split(",");
+   var elemento2 = cad2.substring(1,cad2.length-1).split(",");
+   difference(elemento1, elemento2);
+//});
 }
 
 function cardinalidad(cadena){
