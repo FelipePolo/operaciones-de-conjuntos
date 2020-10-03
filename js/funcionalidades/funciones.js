3d782928964f9f51ef566c27448902fb18ce3ea8
Anedvago <roketrock123@gmail.com>
2020-10-03 11:20:36 -0500

    funcion union que soporta entrada vacio

--- js/funcionalidades/funciones.js
+++ js/funcionalidades/funciones.js
@@ -1,23 +1,46 @@
 // funcion union
 const union = (elemento1, elemento2) => {
+	var resultado = [];
+	resultado.push(elemento1); //a resultado se le asigna el primer vector de elementos
+	//se compara el segundo vector con los elementos del primero, si se encuentra ya no se coloca
+	if(elemento1 == "!"){
+		return `{ ${elemento1} } U { ${elemento2} } = { ${elemento2} }`;
+	}else if(elemento2 == "!"){
+		return `{ ${elemento1} } U { ${elemento2} } = { ${elemento1} }`;
+	}else{
+		for (let i = 0; i < elemento2.length; i++) {
+			let k = true;
+		    for (let j = 0; j < elemento1.length; j++) {
+		    	if (elemento2[i] == elemento1[j]) {
+		    		k = false;
+		    	}
+		    }
+		    if (k == true) {
+		    	resultado.push(elemento2[i]);
+		    }
+		 }
+		  	return `{ ${elemento1} } U { ${elemento2} } = { ${resultado} }`;
+	};
+  }
+  
+
+const interseccion = (elemento1, elemento2) => {
   var resultado = [];
-  resultado.push(elemento1); //a resultado se le asigna el primer vector de elementos
-  //se compara el segundo vector con los elementos del primero, si se encuentra ya no se coloca
   for (let i = 0; i < elemento2.length; i++) {
-    let k = true;
+    let k = false;
     for (let j = 0; j < elemento1.length; j++) {
       if (elemento2[i] == elemento1[j]) {
-        k = false;
+        k = true;
       }
     }
     if (k == true) {
       resultado.push(elemento2[i]);
     }
   }
-  return `{ ${elemento1} } U { ${elemento2} } = { ${resultado} }`;
+  return `{ ${elemento1} } ∩ { ${elemento2} } = { ${resultado} }`;
 };
 
-// funcion diferrencia
+
 const difference = (cadena1, cadena2) => {
   // se convierte en un conjunto dividiendo elementos por la ",", al convertir a conjunto evita que se repitan elemntos dentro de el
   let element1 = cadena1;
@@ -32,16 +55,70 @@ const difference = (cadena1, cadena2) => {
     // conjunto vacío
     result.push("!"); // Este es el simbolo del vacío
   }
-  return result;
+  return  `{ ${cadena1} } - { ${cadena2} } = { ${result} }`;
 };
 
-// funcion cardinalidad
-const cardinalidad = (cadena) => {
-  Size = elemento.length;
-  return Size;
+
+
+const potency = (cadena, num) => {
+  let result = [];
+  result = cadena;
+  for (let i = 0; i < num - 1; i++) {
+    result = concatenation(result, cadena);
+  }
+  return `{ ${result} }`;
 }
-// insertar aqui las demas funciones!
+
+
+const concatenation = (cadena1, cadena2) => {
+  var result = [];
+
+  for (let i = 0; i < cadena1.length; i++) {
+    for (let j = 0; j < cadena2.length; j++) {
+      if (cadena1[i] == "!") {
+        if (!result.includes(cadena2[j])) {
+          result.push(cadena2[j]);
+        }
+      }
+      else if (cadena2[j] == "!") {
+        if (!result.includes(cadena1[j])) {
+          result.push(cadena2[j]);
+        }
+      }
+      else {
+        if (!result.includes(cadena1[i] + cadena2[j])) {
+          result.push(cadena1[i] + cadena2[j]);
+        }
+
+      }
+    }
+  }
+  return result;
+}
+
+const inverse = (cadena) => {
+  let result = [];
+
+  for (let i = 0; i < cadena.length; i++) {
+    let cadenaAux = cadena[i].split("");
+    let aux = "";
+    for (let j = (cadenaAux.length) - 1; j >= 0; j--) {
+      aux += cadenaAux[j];
+    }
+    result.push(aux);
+  }
+  return `{ ${result} }`;
+}
+
+const cardinality = (palabra) => {
+  let array = $("#" + palabra)
+    .val()
+    .split("");
+  return array.length;
+}
+
+
 
 
 //exportando las funciones
-export { union, difference,cardinalidad };
+export { union, difference, concatenation, inverse, potency, cardinality, interseccion };
